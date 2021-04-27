be2edbf1a332b9d84157ef21380ff2d7160b59c7
Ryan Fiorini <ryanwfiorini@hotmail.com>
2021-04-26 23:00:15 -0400

    Added DontUseTheElseKeyword

---
+++ DontUseTheElseKeyword/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DontUseTheElseKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            Console.WriteLine(GetResponseValue_Bad("Second"));
+
+            Console.WriteLine(GetResponseValue_Good("Second"));
+
+            Console.WriteLine();
+            Console.WriteLine("Start");
+        }
+
+        private static string GetResponseValue_Bad(string condition )
+        {
+            if (condition == "first")
+                return "FIRST passed.";
+            else
+                return "FIRST not passed.";
+
+        }
+
+        private static string GetResponseValue_Good(string condition)
+        {
+            if (condition == "first")
+                return "FIRST passed.";
+            
+            return "FIRST not passed.";
+
+        }
+    }
+}
